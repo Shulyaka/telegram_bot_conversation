c570afe592e24621e59297b1e0e40d2424021ffa
Denis Shulyaka <Shulyaka@gmail.com>
2026-04-13 14:42:28 +0300

    Handle conversation errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,7 @@ from homeassistant.components import conversation
 from homeassistant.components.conversation.agent_manager import get_agent_manager
 from homeassistant.components.conversation.models import AbstractConversationAgent
 from homeassistant.components.telegram_bot.const import (
+    ATTR_MEDIA,
     ATTR_PARSER,
     CONF_ALLOWED_CHAT_IDS,
     CONF_API_ENDPOINT,
@@ -127,6 +128,17 @@ def mock_telegram_external_calls() -> Generator[None]:
             super().__init__(*args, **kwargs)
             self._bot_user = test_user
 
+    def mock_send_media_group(*args: Any, **kwargs: Any) -> list[Message]:
+        chat_id: int = args[0]
+        return [
+            Message(
+                message_id=12345 + idx,
+                date=datetime.now(),
+                chat=Chat(id=chat_id, type=ChatType.PRIVATE),
+            )
+            for idx, _ in enumerate(kwargs[ATTR_MEDIA])
+        ]
+
     with (
         patch("homeassistant.components.telegram_bot.bot.Bot", BotMock),
         patch.object(BotMock, "get_chat", return_value=test_chat),
@@ -134,6 +146,7 @@ def mock_telegram_external_calls() -> Generator[None]:
         patch.object(BotMock, "bot", test_user),
         patch.object(BotMock, "send_message", return_value=message),
         patch.object(BotMock, "send_photo", return_value=message),
+        patch.object(BotMock, "send_media_group", side_effect=mock_send_media_group),
         patch.object(BotMock, "send_sticker", return_value=message),
         patch.object(BotMock, "send_video", return_value=message),
         patch.object(BotMock, "send_document", return_value=message),
@@ -141,6 +154,24 @@ def mock_telegram_external_calls() -> Generator[None]:
         patch.object(BotMock, "send_animation", return_value=message),
         patch.object(BotMock, "send_location", return_value=message),
         patch.object(BotMock, "send_poll", return_value=message),
+        patch.object(
+            BotMock,
+            "get_updates",
+            return_value=(
+                Update(
+                    1,
+                    Message(
+                        1,
+                        datetime.now(),
+                        Chat(
+                            id=123456,
+                            type=ChatType.PRIVATE,
+                            first_name="mock first_name",
+                        ),
+                    ),
+                ),
+            ),
+        ),
         patch.object(BotMock, "log_out", return_value=True),
         patch("telegram.ext.Updater._bootstrap"),
     ):
